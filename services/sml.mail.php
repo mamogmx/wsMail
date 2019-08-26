884ca2123f0f715b34e2948010c51356f5ec069c
mamogmx <marco.carbone@gmx.com>
2019-08-26 07:42:22 +0200

    bug fix su inclusione librerie

--- services/sml.mail.php
+++ services/sml.mail.php
@@ -2,6 +2,6 @@
 $baseDir = dirname(dirname(__FILE__));
 error_reporting(E_ERROR);
 require_once $baseDir.DIRECTORY_SEPARATOR."config".DIRECTORY_SEPARATOR."sml.config.php";
-require_once "mail.class.php";
-require_once "wsMail.php";
+require_once $baseDir.DIRECTORY_SEPARATOR."lib".DIRECTORY_SEPARATOR."mail.class.php";
+require_once $baseDir.DIRECTORY_SEPARATOR."lib".DIRECTORY_SEPARATOR."wsMail.php";
 ?>
