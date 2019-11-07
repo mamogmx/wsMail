b995be140bbfb11ef8b6d7fb86c3ecd5289c9e37
Marco Carbone <mamo@gw-iol>
2019-11-07 08:34:00 +0100

    A

--- services/sml.mail.php
+++ services/sml.mail.php
@@ -1,6 +1,7 @@
 <?php
 $baseDir = dirname(dirname(__FILE__));
 error_reporting(E_ERROR);
+ini_set("soap.wsdl_cache_enabled", 0);
 require_once $baseDir.DIRECTORY_SEPARATOR."config".DIRECTORY_SEPARATOR."sml.config.php";
 require_once $baseDir.DIRECTORY_SEPARATOR."lib".DIRECTORY_SEPARATOR."mail.class.php";
 require_once $baseDir.DIRECTORY_SEPARATOR."lib".DIRECTORY_SEPARATOR."wsMail.php";
